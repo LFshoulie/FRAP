7b60279a44b22ba4d6544c05eaa040cdd542fc96
niejiangbo <niejiangbo1229@outlook.com>
2026-03-23 23:10:07 +0800

    继续优化了frap_schedhook中断言的替换

--- nuttx/sched/frap/frap_schedhook.c
+++ nuttx/sched/frap/frap_schedhook.c
@@ -18,7 +18,7 @@
 /****************************************************************************
  * Name: frap_on_preempt
  *
- * 由调度器在发生抢占切换时调用。
+ * 由调度器在发生切换时调用
  *
  * 语义：
  *   如果 oldtcb 正在某个 FRAP 资源的自旋队列中等待（尚未进入
@@ -32,39 +32,42 @@ void frap_on_preempt(FAR struct tcb_s *oldtcb, FAR struct tcb_s *newtcb)
   FAR struct frap_res *r;
   irqstate_t           flags;
 
-  /* 调度器保证不应为空 */
-  DEBUGASSERT(oldtcb != NULL && newtcb != NULL);
+  /* 调度器不应传入 NULL */
+  DEBUGASSERT(oldtcb != NULL);
+  DEBUGASSERT(newtcb != NULL);
 
-  /* 只有更高优先级任务抢占才会影响 FRAP 自旋 */
-
-  if (newtcb->sched_priority <= oldtcb->sched_priority)
+  /* 如果是曾被抢占取消，必须立即将优先级重新提升回 spin_prio */
+  /* if (newtcb->frap_cancelled)
+    {
+      newtcb->frap_queue_preempt_cnt++;
+      newtcb->frap_cancelled = false;
+      frap_set_prio(newtcb, newtcb->frap_spin_prio);
+    } */
+    
+  /* 只有更高优先级任务抢占才会影响 FRAP 自旋；仅当被抢占任务在队列自旋等待时才处理 */
+  if (newtcb->sched_priority <= oldtcb->sched_priority || !oldtcb->frap_enqueued)
     {
       return;
     }
-
-  /* 如果 oldtcb 不在 FRAP 自旋队列中，或者已经在临界段内，则无需处理 */
-  if (!oldtcb->frap_enqueued || oldtcb->frap_in_cs)
-  {
-    return;
-  }
+    
+  /* 在当前实现下，进入临界区后不会发生调度 */
+  DEBUGASSERT(!oldtcb->frap_in_cs);
 
   r = oldtcb->frap_waiting_res;
+  
+  /* enqueued 状态下 waiting_res 必须存在 */
   DEBUGASSERT(r != NULL);
 
   /* 在资源自旋锁保护下安全地将其从 FIFO 中移除 */
-
   flags = spin_lock_irqsave(&r->sl);
 
-  if (oldtcb->frap_enqueued)
-    {
-      frap_queue_remove(r, oldtcb);
-      oldtcb->frap_cancelled = true;
-      /* 被更高优先级任务抢占过一次（由 frap_on_preempt 处理） */
-      oldtcb->frap_queue_preempt_cnt++;
-    }
-    
-    
+  DEBUGASSERT(oldtcb->frap_enqueued);
 
+  /* 被抢占的任务出队，并设置cancled为1 */
+  frap_queue_remove(r, oldtcb);
+  oldtcb->frap_cancelled = true;
+  oldtcb->frap_queue_preempt_cnt++;  // 被抢占次数加一
+  
   spin_unlock_irqrestore(&r->sl, flags);
 
   /* 恢复基准优先级 P_i */
